cd3ee01af083295dc39982542158b36cec6e6ce4
nambur <nambur@wisc.edu>
2014-02-24 23:33:56 -0600

    added local jquery file and added navigation bar for mobile views

--- js/all.js
+++ js/all.js
@@ -1,9 +1,25 @@
 /* general JavaScript functions */
-function searchHover(b)
-{
-	b.setAttribute('src', 'img/search_button_hover.jpg');
-}
-function searchUnhover(b)
-{
-	b.setAttribute('src', 'img/search_button.jpg');
-}
+$(document).ready(function(e) {
+	// navBar click handler for tablet and mobile view
+    $('#mobileNav').on('click', function() {
+		var show = $('.nav_elem').hasClass('hide_tablet');
+		
+		if (show)
+			$('.nav_elem').toggleClass('hide_tablet');
+			
+		$('#navBar').toggleClass('show');
+			
+		$('#navBar').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(e) {
+  			if (!show)
+				$('.nav_elem').toggleClass('hide_tablet');
+		});
+	});
+	
+	// search button handlers
+	$('#searchButton').mouseover( function() {
+		$(this).attr('src', 'img/search_button_hover.jpg');
+	});
+	$('#searchButton').mouseout( function() {
+		$(this).attr('src', 'img/search_button.jpg');
+	});
+});
