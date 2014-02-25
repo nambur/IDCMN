f944a6c4ab66435e618fae9b44702f4f2251afbf
nambur <nambur@wisc.edu>
2014-02-24 19:31:28 -0600

    changed search button imgs, started hide functionality for nav bar as the screen gets smaller

---
+++ js/all.js
@@ -0,0 +1,9 @@
+/* general JavaScript functions */
+function searchHover(b)
+{
+	b.setAttribute('src', 'img/search_button_hover.jpg');
+}
+function searchUnhover(b)
+{
+	b.setAttribute('src', 'img/search_button.jpg');
+}
